0940188590a479f82dfa715481baa553b05081c2
Yuki Matsukura <matsubokkuri@gmail.com>
2024-09-03 15:46:51 +0900

    Revert "Update index.js"
    This reverts commit f3c046454600674e9556a17f03b87282f6e9a630.

--- front/src/index.js
+++ front/src/index.js
@@ -1,5 +1,5 @@
 import StrictMode from 'react';
-import createRoot from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import CSVViewer from './CSVViewer';
 import { Analytics } from "@vercel/analytics/react"
@@ -8,7 +8,7 @@ import UsageImportant from './UsageImportant';
 
 
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <div className="App p-2">
