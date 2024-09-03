6bba34282e26abd585a7bde8e0ffe53e1dc63bec
Yuki Matsukura <matsubo@users.noreply.github.com>
2024-09-03 15:31:33 +0900

    Update index.js

--- front/src/index.js
+++ front/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import StrictMode from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import CSVViewer from './CSVViewer';
@@ -10,12 +10,12 @@ import UsageImportant from './UsageImportant';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <div className="App p-2">
       <UsageImportant/>
       <CSVViewer />
       <Usage/>
       <Analytics/>
     </div>
-  </React.StrictMode>
+  </StrictMode>
 );
